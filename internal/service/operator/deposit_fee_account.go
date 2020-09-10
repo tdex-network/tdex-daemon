89e7941cb9984af8865f0b8b4a9329f9c1990a85
altafan <pietro@vulpem.com>
2020-09-10 10:43:14 +0200

    Fix codestyle in walletservice package

--- internal/service/operator/deposit_fee_account.go
+++ internal/service/operator/deposit_fee_account.go
@@ -11,21 +11,21 @@ import (
 )
 
 // DepositFeeAccount returns a new address for the fee account
-func (s *Service) DepositFeeAccount(ctx context.Context, req *pb.DepositFeeAccountRequest) (*pb.DepositFeeAccountReply, error) {
-	var ctAddress string
-	err := s.vaultRepository.UpdateVault(ctx, func(v *vault.Vault) (*vault.Vault, error) {
+func (s *Service) DepositFeeAccount(ctx context.Context, req *pb.DepositFeeAccountRequest) (reply *pb.DepositFeeAccountReply, err error) {
+	if err = s.vaultRepository.UpdateVault(ctx, func(v *vault.Vault) (*vault.Vault, error) {
 		addr, _, err := v.DeriveNextExternalAddressForAccount(constant.FeeAccount)
 		if err != nil {
 			return nil, err
 		}
-		ctAddress = addr
-		return v, nil
-	})
-	if err != nil {
-		return nil, status.Error(codes.Internal, err.Error())
-	}
 
-	return &pb.DepositFeeAccountReply{
-		Address: ctAddress,
-	}, nil
+		reply = &pb.DepositFeeAccountReply{
+			Address: addr,
+		}
+
+		return v, nil
+	}); err != nil {
+		err = status.Error(codes.Internal, err.Error())
+		return
+	}
+	return
 }
--- internal/service/wallet/change_password.go
+++ internal/service/wallet/change_password.go
@@ -14,14 +14,13 @@ func (s *Service) ChangePassword(ctx context.Context, req *pb.ChangePasswordRequ
 	currentPassphrase := string(req.GetCurrentPassword())
 	newPassphrase := string(req.GetNewPassword())
 
-	err := s.vaultRepository.UpdateVault(ctx, func(v *vault.Vault) (*vault.Vault, error) {
+	if err := s.vaultRepository.UpdateVault(ctx, func(v *vault.Vault) (*vault.Vault, error) {
 		err := v.ChangePassphrase(currentPassphrase, newPassphrase)
 		if err != nil {
 			return nil, err
 		}
 		return v, nil
-	})
-	if err != nil {
+	}); err != nil {
 		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
 
