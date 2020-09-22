60adf5389f61cab49cea448acd96b0c083be036e
altafan <pietro@vulpem.com>
2020-09-22 22:47:09 +0200

    Fix comment and minor fixes

--- internal/service/operator/list_swaps.go
+++ internal/service/operator/list_swaps.go
@@ -3,16 +3,15 @@ package operatorservice
 import (
 	"context"
 
+	"github.com/tdex-network/tdex-daemon/internal/domain/market"
+	"github.com/tdex-network/tdex-daemon/internal/domain/trade"
 	pb "github.com/tdex-network/tdex-protobuf/generated/go/operator"
 	pbtypes "github.com/tdex-network/tdex-protobuf/generated/go/types"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-	"github.com/tdex-network/tdex-daemon/internal/domain/trade"
-	"github.com/tdex-network/tdex-daemon/internal/domain/market"
 )
 
-// ListSwaps returns the list of all swaps processed by the daemon that are in
-// the given status
+// ListSwaps returns the list of all swaps processed by the daemon
 func (s *Service) ListSwaps(ctx context.Context, req *pb.ListSwapsRequest) (*pb.ListSwapsReply, error) {
 	trades, err := s.tradeRepository.GetAllTrades(ctx)
 	if err != nil {
@@ -30,37 +29,39 @@ func (s *Service) ListSwaps(ctx context.Context, req *pb.ListSwapsRequest) (*pb.
 	}, nil
 }
 
-func (s *Service) getMarketsForTrades(ctx context.Context, trades []*trade.Trade) ([]*market.Market, error) {
-	markets := make([]*market.Market, 0, len(trades))
+func (s *Service) getMarketsForTrades(ctx context.Context, trades []*trade.Trade) (map[string]*market.Market, error) {
+	markets := map[string]*market.Market{}
 	for _, trade := range trades {
 		market, _, err := s.marketRepository.GetMarketByAsset(ctx, trade.MarketQuoteAsset())
 		if err != nil {
 			return nil, err
 		}
-		markets = append(markets, market)
+		if _, ok := markets[trade.MarketQuoteAsset()]; !ok {
+			markets[trade.MarketQuoteAsset()] = market
+		}
 	}
 	return markets, nil
 }
 
-func tradesToSwapInfo(markets []*market.Market, trades []*trade.Trade) ([]*pb.SwapInfo) {
+func tradesToSwapInfo(markets map[string]*market.Market, trades []*trade.Trade) []*pb.SwapInfo {
 	info := make([]*pb.SwapInfo, 0, len(trades))
-	for i, trade := range trades {
+	for _, trade := range trades {
 		requestMsg := trade.SwapRequestMessage()
 		fee := &pbtypes.Fee{
-			Asset: markets[i].FeeAsset(),
-			BasisPoint: markets[i].Fee(),
+			Asset:      markets[trade.MarketQuoteAsset()].FeeAsset(),
+			BasisPoint: markets[trade.MarketQuoteAsset()].Fee(),
 		}
 		i := &pb.SwapInfo{
-			Status: trade.Status().Code(),
-			AmountP: requestMsg.GetAmountP(),
-			AssetP: requestMsg.GetAssetP(),
-			AmountR: requestMsg.GetAmountR(),
-			AssetR: requestMsg.GetAssetR(),
-			MarketFee: fee,
-			RequestTimeUnix: trade.SwapRequestTime(),
-			AcceptTimeUnix: trade.SwapAcceptTime(),
+			Status:           trade.Status().Code(),
+			AmountP:          requestMsg.GetAmountP(),
+			AssetP:           requestMsg.GetAssetP(),
+			AmountR:          requestMsg.GetAmountR(),
+			AssetR:           requestMsg.GetAssetR(),
+			MarketFee:        fee,
+			RequestTimeUnix:  trade.SwapRequestTime(),
+			AcceptTimeUnix:   trade.SwapAcceptTime(),
 			CompleteTimeUnix: trade.SwapCompleteTime(),
-			ExpiryTimeUnix: trade.SwapExpiryTime(),
+			ExpiryTimeUnix:   trade.SwapExpiryTime(),
 		}
 		info = append(info, i)
 	}
