2c7491db587936c25e5198b767c20e7424f18c61
Louis Singer <41042567+louisinger@users.noreply.github.com>
2020-10-30 23:05:55 +0100

    Market list RPC in operator_service.go (#150)
    * operator_service.go : add ListMarket rpc funtion
    
    * comment ListMarket rpc
    
    * operator marketList rpc test
    
    * operator test file: hardcode explore url
    
    * better test description + assert that the list is empty
    
    * Remove ListMarketRequest parameter in ListMarket RPC
    
    * Add clean-test in make target
    
    * Move protobuf serialization from application layer to GRPC layer
    
    * operator_service.go, use builtin append instead of assignement
    
    * remove function used to transform MarketInfo
    
    * go back to assignemet
    
    * add a test for ListMarket & not empty market db
    
    * test directory management test_util.go
    
    Co-authored-by: Marco Argentieri <3596602+tiero@users.noreply.github.com>

---
+++ internal/core/application/operator_service_test.go
@@ -0,0 +1,24 @@
+package application
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestListMarket(t *testing.T) {
+	t.Run("ListMarket should return an empty list and a nil error if market repository is empty", func(t *testing.T) {
+		operatorService, ctx, close := newTestOperator(true)
+		marketInfos, err := operatorService.ListMarket(ctx)
+		close()
+		assert.Equal(t, nil, err)
+		assert.Equal(t, 0, len(marketInfos))
+	})
+
+	t.Run("ListMarket should return the number of markets in the market repository", func(t *testing.T) {
+		operatorService, ctx, close := newTestOperator(false)
+		marketInfos, err := operatorService.ListMarket(ctx)
+		close()
+		assert.Equal(t, nil, err)
+		assert.Equal(t, 2, len(marketInfos))
+	})
+}
