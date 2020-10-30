56a973935acbfc734e1880255fa2c1dd4dd6d3d1
altafan <pietro@vulpem.com>
2020-10-30 11:45:46 +0100

    Remove creating/opening/closing markets in bc listener in favour of logs

--- internal/core/application/blockchain_listener.go
+++ internal/core/application/blockchain_listener.go
@@ -58,7 +58,7 @@ func (b *blockchainListener) handleBlockChainEvents() {
 events:
 	for event := range b.crawlerSvc.GetEventChannel() {
 		tx := b.startTx()
-		ctx := context.WithValue(context.Background(), "tx", tx)
+		ctx := context.WithValue(context.Background(), "utx", tx)
 		switch event.Type() {
 		case crawler.FeeAccountDeposit:
 			e := event.(crawler.AddressEvent)
@@ -95,86 +95,37 @@ events:
 					}
 					unspents = append(unspents, u)
 				}
-				err := b.UpdateUnspentsForAddress(
-					ctx,
-					unspents,
-					e.Address,
-				)
-				if err != nil {
-					tx.Discard()
-					log.Warn(err)
-					continue events
-				}
-
-				markets, err := b.marketRepository.GetTradableMarkets(
-					ctx,
-				)
-				if err != nil {
+				if err := b.UpdateUnspentsForAddress(ctx, unspents, e.Address); err != nil {
 					tx.Discard()
 					log.Warn(err)
 					continue events
 				}
 
 				addresses, _, err := b.vaultRepository.
-					GetAllDerivedAddressesAndBlindingKeysForAccount(
-						ctx,
-						domain.FeeAccount,
-					)
+					GetAllDerivedAddressesAndBlindingKeysForAccount(ctx, domain.FeeAccount)
 				if err != nil {
 					tx.Discard()
 					log.Warn(err)
 					continue events
 				}
 
-				var feeAccountBalance uint64
-				for _, a := range addresses {
-					b, err := b.unspentRepository.GetBalance(
-						ctx,
-						a,
-						config.GetString(config.BaseAssetKey),
-					)
-					if err != nil {
-						tx.Discard()
-						log.Warn(err)
-						continue events
-					}
-
-					feeAccountBalance += b
-				}
-
-				if feeAccountBalance < uint64(
-					config.GetInt(config.FeeAccountBalanceThresholdKey),
-				) {
-					log.Debug("fee account balance too low - Trades and" +
-						" deposits will be disabled")
-					for _, m := range markets {
-						err := b.marketRepository.CloseMarket(
-							ctx,
-							m.QuoteAsset,
-						)
-						if err != nil {
-							tx.Discard()
-							log.Warn(err)
-							continue events
-						}
-					}
+				feeAccountBalance, err := b.unspentRepository.GetBalance(
+					ctx,
+					addresses,
+					config.GetString(config.BaseAssetKey),
+				)
+				if err != nil {
+					tx.Discard()
+					log.Warn(err)
 					continue events
 				}
 
-				for _, m := range markets {
-					err := b.marketRepository.OpenMarket(
-						ctx,
-						m.QuoteAsset,
+				if feeAccountBalance < uint64(config.GetInt(config.FeeAccountBalanceThresholdKey)) {
+					log.Debug(
+						"fee account balance too low - Trades and deposits must be " +
+							"disabled until account is refunded",
 					)
-					if err != nil {
-						tx.Discard()
-						log.Warn(err)
-						continue events
-					}
-					log.Debug(fmt.Sprintf(
-						"market %v, opened",
-						m.AccountIndex,
-					))
+					continue events
 				}
 			}
 
@@ -204,9 +155,6 @@ events:
 						RangeProof:      utxo.RangeProof(),
 						SurjectionProof: utxo.SurjectionProof(),
 						Address:         e.Address,
-						Spent:           false,
-						Locked:          false,
-						LockedBy:        nil,
 						Confirmed:       isTrxConfirmed,
 					}
 					unspents = append(unspents, u)
@@ -222,60 +170,56 @@ events:
 					continue events
 				}
 
-				fundingTxs := make([]domain.OutpointWithAsset, 0)
-				for _, u := range e.Utxos {
-					tx := domain.OutpointWithAsset{
-						Asset: u.Asset(),
-						Txid:  u.Hash(),
-						Vout:  int(u.Index()),
-					}
-					fundingTxs = append(fundingTxs, tx)
-				}
-
-				m, err := b.marketRepository.GetOrCreateMarket(
-					ctx,
-					e.AccountIndex,
-				)
+				market, err := b.marketRepository.GetMarketByAccount(ctx, e.AccountIndex)
 				if err != nil {
-					tx.Discard()
-					log.Error(err)
-					continue events
-				}
-
-				if err := b.marketRepository.UpdateMarket(
-					ctx,
-					m.AccountIndex,
-					func(m *domain.Market) (*domain.Market, error) {
-
-						if m.IsFunded() {
-							return m, nil
-						}
-
-						if err := m.FundMarket(fundingTxs); err != nil {
-							tx.Discard()
-							return nil, err
-						}
-
-						if m.IsFunded() {
-							log.Info(
-								"deposit: funding market with quote asset ",
-								m.QuoteAsset,
-							)
-						} else {
-							assetType := "base"
-							if len(m.QuoteAsset) <= 0 {
-								assetType = "quote"
-							}
-							log.Warn(fmt.Sprintf("%s asset is missing", assetType))
-						}
-
-						return m, nil
-					}); err != nil {
-					tx.Discard()
 					log.Warn(err)
-					continue events
 				}
 
+				// if market is not found it means it's never been opened, therefore
+				// let's notify whether the market can be safely opened, base or quote
+				// asset are missing, or if the market account owns too many assets.
+				if market == nil || !market.IsFunded() {
+					addresses, _, err := b.vaultRepository.GetAllDerivedAddressesAndBlindingKeysForAccount(ctx, e.AccountIndex)
+					if err != nil {
+						log.Warn(err)
+					}
+					unspents, err := b.unspentRepository.GetUnspentsForAddresses(ctx, addresses)
+					if err != nil {
+						log.Warn(err)
+					}
+					unspentsAssetType := map[string]bool{}
+					for _, u := range unspents {
+						unspentsAssetType[u.AssetHash] = true
+					}
+
+					switch len(unspentsAssetType) {
+					case 0:
+						log.Warn("no funds detected for market")
+					case 1:
+						asset := "base"
+						for k := range unspentsAssetType {
+							if k == config.GetString(config.BaseAssetKey) {
+								asset = "quote"
+							}
+						}
+						log.Warn(fmt.Sprintf("%s asset is missing", asset))
+					case 2:
+						var asset string
+						for k := range unspentsAssetType {
+							if k != config.GetString(config.BaseAssetKey) {
+								asset = k
+							}
+						}
+						log.Infof("market with quote asset '%s' can be opened", asset)
+					default:
+						log.Warn(
+							"market account funded with more than 2 different assets." +
+								"It will be impossible to determine the correct quote asset " +
+								"and market won't be opened. Funds must be moved away from " +
+								"this account so that it owns only unspents of 2 type of assets",
+						)
+					}
+				}
 			}
 
 		case crawler.TransactionConfirmed:
@@ -290,7 +234,7 @@ func (b *blockchainListener) UpdateUnspentsForAddress(
 	unspents []domain.Unspent,
 	address string,
 ) error {
-	existingUnspents, err := b.unspentRepository.GetUnspentsForAddresses(
+	existingUnspents, err := b.unspentRepository.GetAllUnspentsForAddresses(
 		ctx,
 		[]string{address},
 	)
@@ -302,53 +246,44 @@ func (b *blockchainListener) UpdateUnspentsForAddress(
 	unspentsToAdd := make([]domain.Unspent, 0)
 	for _, newUnspent := range unspents {
 		exist := false
-	existing:
 		for _, existingUnspent := range existingUnspents {
 			if newUnspent.IsKeyEqual(existingUnspent.Key()) {
 				exist = true
-				break existing
+				break
 			}
 		}
 		if !exist {
 			unspentsToAdd = append(unspentsToAdd, newUnspent)
 		}
 	}
-	err = b.unspentRepository.AddUnspents(ctx, unspentsToAdd)
-	if err != nil {
+
+	if err := b.unspentRepository.AddUnspents(ctx, unspentsToAdd); err != nil {
 		return err
 	}
 
 	//update spent
+	unspentsToMarkAsSpent := make([]domain.UnspentKey, 0)
 	for _, existingUnspent := range existingUnspents {
 		exist := false
-	newUnsp:
 		for _, newUnspent := range unspents {
-			if newUnspent.IsKeyEqual(existingUnspent.Key()) {
+			if existingUnspent.IsKeyEqual(newUnspent.Key()) {
 				exist = true
-				break newUnsp
+				break
 			}
 		}
 		if !exist {
-			existingUnspent.Spend()
-			err := b.unspentRepository.UpdateUnspent(
-				ctx,
-				existingUnspent.Key(),
-				func(unspent *domain.Unspent) (*domain.Unspent, error) {
-					unspent.Spend()
-					return unspent, nil
-				},
-			)
-			if err != nil {
-				return err
-			}
+			unspentsToMarkAsSpent = append(unspentsToMarkAsSpent, existingUnspent.Key())
 		}
 	}
 
+	if err := b.unspentRepository.SpendUnspents(ctx, unspentsToMarkAsSpent); err != nil {
+		return err
+	}
 	return nil
 }
 
 func (b *blockchainListener) startTx() ports.Transaction {
-	return b.dbManager.NewTransaction()
+	return b.dbManager.NewUnspentsTransaction()
 }
 
 func (b *blockchainListener) commitTx(tx ports.Transaction) {
