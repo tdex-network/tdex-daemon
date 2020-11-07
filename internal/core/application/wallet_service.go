6157f57abca4a50dc09d7defda373878271d7b42
altafan <pietro@vulpem.com>
2020-11-07 02:48:00 +0100

    Add test for SendToMany

--- internal/core/application/wallet_service.go
+++ internal/core/application/wallet_service.go
@@ -205,7 +205,8 @@ func (w *walletService) UnlockWallet(
 func (w *walletService) ChangePassword(
 	ctx context.Context,
 	currentPassphrase string,
-	newPassphrase string) error {
+	newPassphrase string,
+) error {
 	if w.walletIsSyncing {
 		return ErrWalletIsSyncing
 	}
