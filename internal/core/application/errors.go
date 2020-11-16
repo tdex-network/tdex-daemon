3854f5f331a3c0ef1750dedea202a4905f2abaf9
Louis Singer <41042567+louisinger@users.noreply.github.com>
2020-11-16 12:10:25 +0100

    Update internal/core/application/errors.go
    Co-authored-by: Marco Argentieri <3596602+tiero@users.noreply.github.com>

--- internal/core/application/errors.go
+++ internal/core/application/errors.go
@@ -3,4 +3,4 @@ package application
 import "errors"
 
 // ErrCrawlerDoesNotObserveAddresses occurs when the crawler does not observe any address
-var	ErrCrawlerDoesNotObserveAddresses = errors.New("crawler does not observe any addresses")
+var	ErrCrawlerDoesNotObserveFeeAccount = errors.New("fee account needs to be funded to open a market")
