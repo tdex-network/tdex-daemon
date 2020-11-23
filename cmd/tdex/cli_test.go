d6580047ea08bde24a978eba439f9c61faeef34a
louisinger <louis@vulpem.com>
2020-11-23 10:29:47 +0100

    test open market

--- cmd/tdex/cli_test.go
+++ cmd/tdex/cli_test.go
@@ -15,7 +15,12 @@ import (
 	"github.com/tdex-network/tdex-daemon/pkg/explorer"
 )
 
-const password = "hodlhodlhodl"
+const (
+	password       = "hodlhodlhodl"
+	emptyAsset     = ""
+	LBTC           = "5ac9f65c0efcc4775e0baec4ec03abdde22473cd3cf33c0419ca290e0751b225"
+	nigiriEndpoint = "https://nigiri.network/liquid/api"
+)
 
 func TestGenSeed(t *testing.T) {
 	container := runNewContainer(t)
@@ -67,25 +72,10 @@ func TestCreateMarket(t *testing.T) {
 	container := runNewContainer(t)
 	defer stopAndDeleteContainer(container)
 
-	// init the wallet
-	seed, err := runCLICommand(container, "genseed")
-	if err != nil {
-		t.Error(err)
-	}
-
-	_, err = runCLICommand(container, "init", "--seed", seed, "--password", password)
-	if err != nil {
-		t.Error(err)
-	}
-
-	_, err = runCLICommand(container, "unlock", "--password", password)
-	if err != nil {
-		t.Error(err)
-	}
+	initAndUnlockWallet(t, container)
 
 	// Create the market and store the address to found
 	var depositMarketResult map[string]interface{}
-	const emptyAsset = ""
 	depositMarketJson, err := runCLICommand(container, "depositmarket", "--base_asset", emptyAsset, "--quote_asset", emptyAsset)
 	if err != nil {
 		t.Error(t, err)
@@ -98,44 +88,14 @@ func TestCreateMarket(t *testing.T) {
 
 	address := depositMarketResult["address"].(string)
 
-	listMarket := func() []map[string]interface{} {
-		var listMarketResult map[string][]map[string]interface{}
-
-		result, err := runCLICommand(container, "listmarket")
-		if err != nil {
-			t.Error(err)
-		}
-
-		err = json.Unmarshal([]byte(result), &listMarketResult)
-		if err != nil {
-			t.Error(err)
-		}
-
-		markets := listMarketResult["markets"]
-
-		return markets
-	}
-
 	t.Run("should create a market new market", func(t *testing.T) {
-		markets := listMarket()
+		markets := listMarket(t, container)
 		assert.Equal(t, 1, len(markets))
 	})
 
-	t.Run("should fund the market if the market's address is founded", func(t *testing.T) {
-		explorerSvc := explorer.NewService("https://nigiri.network/liquid/api")
-		_, err := explorerSvc.Faucet(address)
-		if err != nil {
-			t.Error(err)
-		}
-
-		_, _, err = explorerSvc.Mint(address, 100)
-		if err != nil {
-			t.Error(err)
-		}
-
-		time.Sleep(3 * time.Second)
-
-		market := listMarket()[0]["market"].(map[string]interface{})
+	t.Run("should fill the base and quote assets if the market's address is founded", func(t *testing.T) {
+		fundMarketAddress(t, address)
+		market := listMarket(t, container)[0]["market"].(map[string]interface{})
 		assert.NotNil(t, market["base_asset"])
 		assert.NotNil(t, market["quote_asset"])
 	})
@@ -145,6 +105,103 @@ func TestDepositFee(t *testing.T) {
 	container := runNewContainer(t)
 	defer stopAndDeleteContainer(container)
 
+	initAndUnlockWallet(t, container)
+
+	t.Run("should return an blinding address", func(t *testing.T) {
+		depositFeeResult, err := depositFeeAccount(t, container)
+		assert.Nil(t, err)
+		assert.NotNil(t, depositFeeResult["address"])
+		assert.NotNil(t, depositFeeResult["blinding"])
+	})
+}
+
+func TestOpenMarket(t *testing.T) {
+	init := func() (string, string) {
+		container := runNewContainer(t)
+
+		initAndUnlockWallet(t, container)
+
+		// Create the market and store the address to found
+		var depositMarketResult map[string]interface{}
+		depositMarketJson, err := runCLICommand(container, "depositmarket", "--base_asset", emptyAsset, "--quote_asset", emptyAsset)
+		if err != nil {
+			t.Error(t, err)
+		}
+
+		err = json.Unmarshal([]byte(depositMarketJson), &depositMarketResult)
+		if err != nil {
+			t.Error(t, err)
+		}
+
+		address := depositMarketResult["address"].(string)
+		shitcoin := fundMarketAddress(t, address)
+
+		return container, shitcoin
+	}
+
+	t.Run("should make a market tradable", func(t *testing.T) {
+		container, shitcoin := init()
+		defer stopAndDeleteContainer(container)
+		// first select the market
+		_, err := runCLICommand(container, "market", "--base_asset", LBTC, "--quote_asset", shitcoin)
+		if err != nil {
+			t.Error(err)
+		}
+
+		depositFeeResult, err := depositFeeAccount(t, container)
+		if err != nil {
+			t.Error(err)
+		}
+
+		faucetAddress(t, depositFeeResult["address"])
+
+		// open the selected market
+		_, err = runCLICommand(container, "open")
+		assert.Nil(t, err)
+
+		market := listMarket(t, container)[0]["tradable"].(bool)
+		assert.Equal(t, true, market)
+	})
+
+	t.Run("should return an error if any market is selected", func(t *testing.T) {
+		container, _ := init()
+		defer stopAndDeleteContainer(container)
+		// open the selected market
+		_, err := runCLICommand(container, "open")
+		assert.NotNil(t, err)
+	})
+}
+
+func listMarket(t *testing.T, container string) []map[string]interface{} {
+	var listMarketResult map[string][]map[string]interface{}
+
+	result, err := runCLICommand(container, "listmarket")
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = json.Unmarshal([]byte(result), &listMarketResult)
+	if err != nil {
+		t.Error(err)
+	}
+
+	markets := listMarketResult["markets"]
+
+	return markets
+}
+
+func depositFeeAccount(t *testing.T, container string) (map[string]string, error) {
+	var depositFeeResult map[string]string
+	depositFeeJson, err := runCLICommand(container, "depositfee")
+	errUnmarshal := json.Unmarshal([]byte(depositFeeJson), &depositFeeResult)
+	if errUnmarshal != nil {
+		t.Error(errUnmarshal)
+	}
+
+	return depositFeeResult, err
+}
+
+func initAndUnlockWallet(t *testing.T, container string) {
 	// init the wallet
 	seed, err := runCLICommand(container, "genseed")
 	if err != nil {
@@ -160,19 +217,33 @@ func TestDepositFee(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
+}
 
-	t.Run("should return an blinding address", func(t *testing.T) {
-		var depositFeeResult map[string]string
-		depositFeeJson, err := runCLICommand(container, "depositfee")
-		errUnmarshal := json.Unmarshal([]byte(depositFeeJson), &depositFeeResult)
-		if errUnmarshal != nil {
-			t.Error(errUnmarshal)
-		}
+// returns the shitcoin asset hash generated
+func fundMarketAddress(t *testing.T, address string) string {
+	explorerSvc := explorer.NewService(nigiriEndpoint)
+	_, err := explorerSvc.Faucet(address)
+	if err != nil {
+		t.Error(err)
+	}
 
-		assert.Nil(t, err)
-		assert.NotNil(t, depositFeeResult["address"])
-		assert.NotNil(t, depositFeeResult["blinding"])
-	})
+	_, shitcoin, err := explorerSvc.Mint(address, 100)
+	if err != nil {
+		t.Error(err)
+	}
+
+	time.Sleep(3 * time.Second)
+	return shitcoin
+}
+
+func faucetAddress(t *testing.T, address string) {
+	explorerSvc := explorer.NewService(nigiriEndpoint)
+	_, err := explorerSvc.Faucet(address)
+	if err != nil {
+		t.Error(err)
+	}
+
+	time.Sleep(3 * time.Second)
 }
 
 func execute(command string, args ...string) (string, error) {
