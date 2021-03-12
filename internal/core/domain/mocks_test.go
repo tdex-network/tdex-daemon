8f6aae60c7ebcaa7ed60573b020b7784b3872e94
altafan <pietro@vulpem.com>
2021-03-12 20:01:09 +0100

    Refactor domain & repo tests

---
+++ internal/core/domain/mocks_test.go
@@ -0,0 +1,203 @@
+package domain_test
+
+import (
+	"errors"
+
+	"github.com/google/uuid"
+	"github.com/tdex-network/tdex-daemon/internal/core/domain"
+	"github.com/vulpemventures/go-elements/network"
+)
+
+/*
+ * SwapParser
+ */
+type mockSwapParser struct {
+	mustFail bool
+}
+
+func newMockedSwapParser(mustFail bool) domain.ISwapParser {
+	return mockSwapParser{mustFail}
+}
+
+func (m mockSwapParser) SerializeRequest(_ domain.SwapRequest) ([]byte, *domain.SwapError) {
+	if m.mustFail {
+		return nil, &domain.SwapError{errors.New("something went wrong"), 10}
+	}
+
+	return make([]byte, 20), nil
+}
+
+func (m mockSwapParser) SerializeAccept(_ domain.AcceptArgs) (string, []byte, *domain.SwapError) {
+	if m.mustFail {
+		return "", nil, &domain.SwapError{errors.New("something went wrong"), 10}
+	}
+
+	return uuid.New().String(), make([]byte, 20), nil
+}
+
+func (m mockSwapParser) SerializeComplete(_ domain.SwapRequest, _ domain.SwapAccept, _ string) (string, []byte, *domain.SwapError) {
+	if m.mustFail {
+		return "", nil, &domain.SwapError{errors.New("something went wrong"), 10}
+	}
+
+	return uuid.New().String(), make([]byte, 20), nil
+}
+
+func (m mockSwapParser) SerializeFail(_ string, _ int, _ string) (string, []byte) {
+	return uuid.New().String(), make([]byte, 20)
+}
+
+func (m mockSwapParser) DeserializeRequest(_ []byte) (domain.SwapRequest, error) {
+	if m.mustFail {
+		return nil, errors.New("something went wrong")
+	}
+
+	return newMockedSwapRequest(), nil
+}
+
+func (m mockSwapParser) DeserializeAccept(_ []byte) (domain.SwapAccept, error) {
+	if m.mustFail {
+		return nil, errors.New("something went wrong")
+	}
+
+	return newMockedSwapAccept(), nil
+}
+
+func (m mockSwapParser) DeserializeComplete(_ []byte) (domain.SwapComplete, error) {
+	if m.mustFail {
+		return nil, errors.New("something went wrong")
+	}
+
+	return newMockedSwapComplete(), nil
+}
+
+func (m mockSwapParser) DeserializeFail(_ []byte) (domain.SwapFail, error) {
+	return nil, nil
+}
+
+/*
+ * PsetManager
+ */
+type mockPsetManager struct{}
+
+func newMockedPsetManager() domain.IPsetManager {
+	return mockPsetManager{}
+}
+
+func (m mockPsetManager) GetTxID(_ string) (string, error) {
+	return randomHex(32), nil
+}
+
+func (m mockPsetManager) GetTxHex(_ string) (string, string, error) {
+	return randomHex(100), randomHex(32), nil
+}
+
+/*
+ * SwapRequest
+ */
+type mockSwapRequest struct {
+	id string
+}
+
+func newMockedSwapRequest() domain.SwapRequest {
+	return &mockSwapRequest{uuid.New().String()}
+}
+
+func (m mockSwapRequest) GetId() string {
+	return m.id
+}
+
+func (m mockSwapRequest) GetAssetP() string {
+	return network.Regtest.AssetID
+}
+
+func (m mockSwapRequest) GetAmountP() uint64 {
+	return 10000000
+}
+
+func (m mockSwapRequest) GetAssetR() string {
+	return randomHex(32)
+}
+
+func (m mockSwapRequest) GetAmountR() uint64 {
+	return 300000000000
+}
+
+func (m mockSwapRequest) GetTransaction() string {
+	return randomBase64(100)
+}
+
+func (m mockSwapRequest) GetInputBlindingKey() map[string][]byte {
+	mm := map[string][]byte{}
+	mm[randomHex(20)] = randomBytes(32)
+	return mm
+}
+
+func (m mockSwapRequest) GetOutputBlindingKey() map[string][]byte {
+	mm := map[string][]byte{}
+	mm[randomHex(20)] = randomBytes(32)
+	mm[randomHex(20)] = randomBytes(32)
+	return mm
+}
+
+/*
+ * SwapAccept
+ */
+type mockSwapAccept struct {
+	id string
+}
+
+func newMockedSwapAccept() domain.SwapAccept {
+	return mockSwapAccept{uuid.New().String()}
+}
+
+func (m mockSwapAccept) GetId() string {
+	return m.id
+}
+
+func (m mockSwapAccept) GetRequestId() string {
+	return uuid.New().String()
+}
+
+func (m mockSwapAccept) GetTransaction() string {
+	return randomBase64(100)
+}
+
+func (m mockSwapAccept) GetInputBlindingKey() map[string][]byte {
+	mm := map[string][]byte{}
+	mm[randomHex(20)] = randomBytes(32)
+	mm[randomHex(20)] = randomBytes(32)
+	return mm
+}
+
+func (m mockSwapAccept) GetOutputBlindingKey() map[string][]byte {
+	mm := map[string][]byte{}
+	mm[randomHex(20)] = randomBytes(32)
+	mm[randomHex(20)] = randomBytes(32)
+	mm[randomHex(20)] = randomBytes(32)
+	mm[randomHex(20)] = randomBytes(32)
+	return mm
+}
+
+/*
+ * SwapComplete
+ */
+type mockSwapComplete struct {
+	id string
+}
+
+func newMockedSwapComplete() domain.SwapComplete {
+	return &mockSwapComplete{uuid.New().String()}
+}
+
+func (m mockSwapComplete) GetId() string {
+	return m.id
+}
+
+func (m mockSwapComplete) GetAcceptId() string {
+	return uuid.New().String()
+}
+
+func (m mockSwapComplete) GetTransaction() string {
+	return randomBase64(100)
+}
---
+++ internal/infrastructure/storage/db/test/trade_repository_impl_test.go
@@ -0,0 +1,321 @@
+package db_test
+
+import (
+	"context"
+	"errors"
+	"os"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+	"github.com/tdex-network/tdex-daemon/internal/core/domain"
+	"github.com/tdex-network/tdex-daemon/internal/core/ports"
+	dbbadger "github.com/tdex-network/tdex-daemon/internal/infrastructure/storage/db/badger"
+	"github.com/tdex-network/tdex-daemon/internal/infrastructure/storage/db/inmemory"
+)
+
+func TestTradeRepositoryImplementations(t *testing.T) {
+	repositories, cancel := createTradeRepositories(t)
+	t.Cleanup(cancel)
+
+	for i := range repositories {
+		repo := repositories[i]
+
+		t.Run(repo.Name, func(t *testing.T) {
+			t.Parallel()
+
+			t.Run("testGetOrCreateTrade", func(t *testing.T) {
+				t.Parallel()
+				testGetOrCreateTrade(t, repo)
+			})
+
+			t.Run("testGetAllTrades", func(t *testing.T) {
+				t.Parallel()
+				testGetAllTrades(t, repo)
+			})
+
+			t.Run("testGetAllTradesForMarket", func(t *testing.T) {
+				t.Parallel()
+				testGetAllTradesForMarket(t, repo)
+			})
+
+			t.Run("testGetCompletedTradesForMarket", func(t *testing.T) {
+				t.Parallel()
+				testGetCompletedTradesForMarket(t, repo)
+			})
+
+			t.Run("testGetTradeWithSwapAcceptID", func(t *testing.T) {
+				t.Parallel()
+				testGetTradeWithSwapAcceptID(t, repo)
+			})
+
+			// TODO: uncomment - the following test demonstrate that in case of error,
+			// any change to the db is rolled back. Currently, the transactional
+			// component is not properly implemented for inmemory DbManager and the
+			// test below would fail for te inmemory implementation.
+
+			// t.Run("testUpdateTrade_rollback", func(t *testing.T) {
+			// 	t.Parallel()
+			// 	testUpdateTradeRollback(t, repo)
+			// })
+		})
+	}
+}
+
+func testGetOrCreateTrade(t *testing.T, repo tradeRepository) {
+	iTrade, err := repo.write(func(ctx context.Context) (interface{}, error) {
+		return repo.Repository.GetOrCreateTrade(ctx, nil)
+	})
+	require.NoError(t, err)
+	trade, ok := iTrade.(*domain.Trade)
+	require.True(t, ok)
+	require.NotNil(t, trade)
+}
+
+func testGetAllTrades(t *testing.T, repo tradeRepository) {
+	iTrades, err := repo.write(func(ctx context.Context) (interface{}, error) {
+		_, err := repo.Repository.GetOrCreateTrade(ctx, nil)
+		if err != nil {
+			return nil, err
+		}
+		return repo.Repository.GetAllTrades(ctx)
+	})
+	require.NoError(t, err)
+	trades, ok := iTrades.([]*domain.Trade)
+	require.True(t, ok)
+	require.GreaterOrEqual(t, len(trades), 1)
+}
+
+func testGetAllTradesForMarket(t *testing.T, repo tradeRepository) {
+	marketAsset := randomString(32)
+	var tradeID uuid.UUID
+
+	iTrades, err := repo.write(func(ctx context.Context) (interface{}, error) {
+		trade, err := repo.Repository.GetOrCreateTrade(ctx, nil)
+		if err != nil {
+			return nil, err
+		}
+		tradeID = trade.ID
+		return repo.Repository.GetAllTradesForMarket(ctx, marketAsset)
+	})
+	require.NoError(t, err)
+	trades, ok := iTrades.([]*domain.Trade)
+	require.True(t, ok)
+	require.Len(t, trades, 0)
+
+	iTrades, err = repo.write(func(ctx context.Context) (interface{}, error) {
+		if err := repo.Repository.UpdateTrade(
+			ctx,
+			&tradeID,
+			func(trade *domain.Trade) (*domain.Trade, error) {
+				trade.MarketQuoteAsset = marketAsset
+				return trade, nil
+			},
+		); err != nil {
+			return nil, err
+		}
+		return repo.Repository.GetAllTradesForMarket(ctx, marketAsset)
+	})
+	require.NoError(t, err)
+	trades, ok = iTrades.([]*domain.Trade)
+	require.True(t, ok)
+	require.GreaterOrEqual(t, len(trades), 1)
+}
+
+func testGetCompletedTradesForMarket(t *testing.T, repo tradeRepository) {
+	marketAsset := randomString(32)
+	var tradeID uuid.UUID
+
+	iTrades, err := repo.write(func(ctx context.Context) (interface{}, error) {
+		trade, err := repo.Repository.GetOrCreateTrade(ctx, nil)
+		if err != nil {
+			return nil, err
+		}
+		if err := repo.Repository.UpdateTrade(
+			ctx,
+			&trade.ID,
+			func(trade *domain.Trade) (*domain.Trade, error) {
+				trade.MarketQuoteAsset = marketAsset
+				return trade, nil
+			},
+		); err != nil {
+			return nil, err
+		}
+		tradeID = trade.ID
+		return repo.Repository.GetCompletedTradesForMarket(ctx, marketAsset)
+	})
+	require.NoError(t, err)
+	trades, ok := iTrades.([]*domain.Trade)
+	require.True(t, ok)
+	require.Len(t, trades, 0)
+
+	iTrades, err = repo.write(func(ctx context.Context) (interface{}, error) {
+		if err := repo.Repository.UpdateTrade(
+			ctx,
+			&tradeID,
+			func(trade *domain.Trade) (*domain.Trade, error) {
+				trade.Status = domain.CompletedStatus
+				return trade, nil
+			},
+		); err != nil {
+			return nil, err
+		}
+		return repo.Repository.GetCompletedTradesForMarket(ctx, marketAsset)
+	})
+	require.NoError(t, err)
+	trades, ok = iTrades.([]*domain.Trade)
+	require.True(t, ok)
+	require.GreaterOrEqual(t, len(trades), 1)
+
+	iTrades, err = repo.write(func(ctx context.Context) (interface{}, error) {
+		trade, err := repo.Repository.GetOrCreateTrade(ctx, nil)
+		if err != nil {
+			return nil, err
+		}
+		if err := repo.Repository.UpdateTrade(
+			ctx,
+			&trade.ID,
+			func(trade *domain.Trade) (*domain.Trade, error) {
+				trade.MarketQuoteAsset = marketAsset
+				trade.Status = domain.SettledStatus
+				return trade, nil
+			},
+		); err != nil {
+			return nil, err
+		}
+		return repo.Repository.GetCompletedTradesForMarket(ctx, marketAsset)
+	})
+	require.NoError(t, err)
+	trades, ok = iTrades.([]*domain.Trade)
+	require.True(t, ok)
+	require.GreaterOrEqual(t, len(trades), 2)
+}
+
+func testGetTradeWithSwapAcceptID(t *testing.T, repo tradeRepository) {
+	swapAcceptID := uuid.New().String()
+
+	iTrades, err := repo.write(func(ctx context.Context) (interface{}, error) {
+		if _, err := repo.Repository.GetOrCreateTrade(ctx, nil); err != nil {
+			return nil, err
+		}
+		return repo.Repository.GetTradeWithSwapAcceptID(ctx, swapAcceptID)
+	})
+	require.NoError(t, err)
+	require.Nil(t, iTrades)
+}
+
+func testGetTradeWithTxID(t *testing.T, repo tradeRepository) {
+	txId := randomString(32)
+	var tradeId uuid.UUID
+
+	iTrades, err := repo.write(func(ctx context.Context) (interface{}, error) {
+		trade, err := repo.Repository.GetOrCreateTrade(ctx, nil)
+		if err != nil {
+			return nil, err
+		}
+		tradeId = trade.ID
+		return repo.Repository.GetTradeWithTxID(ctx, txId)
+	})
+	require.NoError(t, err)
+	trades, ok := iTrades.([]*domain.Trade)
+	require.True(t, ok)
+	require.Len(t, trades, 0)
+
+	iTrades, err = repo.write(func(ctx context.Context) (interface{}, error) {
+		if err := repo.Repository.UpdateTrade(
+			ctx,
+			&tradeId,
+			func(trade *domain.Trade) (*domain.Trade, error) {
+				trade.TxID = txId
+				return trade, nil
+			},
+		); err != nil {
+			return nil, err
+		}
+		return repo.Repository.GetTradeWithTxID(ctx, txId)
+	})
+	require.NoError(t, err)
+	trades, ok = iTrades.([]*domain.Trade)
+	require.True(t, ok)
+	require.GreaterOrEqual(t, len(trades), 1)
+}
+
+func testUpdateTradeRollback(t *testing.T, repo tradeRepository) {
+	expectedErr := errors.New("something went wrong")
+	var tradeID uuid.UUID
+
+	trade, err := repo.write(func(ctx context.Context) (interface{}, error) {
+		trade, err := repo.Repository.GetOrCreateTrade(ctx, nil)
+		if err != nil {
+			return nil, err
+		}
+		tradeID = trade.ID
+
+		if err := repo.Repository.UpdateTrade(
+			ctx,
+			&trade.ID,
+			func(v *domain.Trade) (*domain.Trade, error) {
+				return nil, expectedErr
+			},
+		); err != nil {
+			return nil, err
+		}
+
+		return repo.Repository.GetOrCreateTrade(ctx, &tradeID)
+	})
+	require.EqualError(t, err, expectedErr.Error())
+	require.Nil(t, trade)
+
+	trade, err = repo.read(func(ctx context.Context) (interface{}, error) {
+		return repo.Repository.GetOrCreateTrade(ctx, &tradeID)
+	})
+	require.Error(t, err)
+	require.Nil(t, trade)
+}
+
+func createTradeRepositories(t *testing.T) ([]tradeRepository, func()) {
+	datadir := "tradedb"
+	err := os.Mkdir(datadir, os.ModePerm)
+	require.NoError(t, err)
+
+	inmemoryDBManager := inmemory.NewDbManager()
+	badgerDBManager, err := dbbadger.NewDbManager(datadir, nil)
+	require.NoError(t, err)
+
+	return []tradeRepository{
+			{
+				Name:       "badger",
+				DBManager:  badgerDBManager,
+				Repository: newBadgerTradeRepository(badgerDBManager),
+			},
+			{
+				Name:       "inmemory",
+				DBManager:  inmemoryDBManager,
+				Repository: newInMemoryTradeRepository(inmemoryDBManager),
+			},
+		}, func() {
+			os.RemoveAll(datadir)
+		}
+}
+
+func newBadgerTradeRepository(dbmanager *dbbadger.DbManager) domain.TradeRepository {
+	return dbbadger.NewTradeRepositoryImpl(dbmanager)
+}
+
+func newInMemoryTradeRepository(dbmanager *inmemory.DbManager) domain.TradeRepository {
+	return inmemory.NewTradeRepositoryImpl(dbmanager)
+}
+
+type tradeRepository struct {
+	Name       string
+	DBManager  ports.DbManager
+	Repository domain.TradeRepository
+}
+
+func (r tradeRepository) read(query func(context.Context) (interface{}, error)) (interface{}, error) {
+	return r.DBManager.RunTransaction(context.Background(), true, query)
+}
+
+func (r tradeRepository) write(query func(context.Context) (interface{}, error)) (interface{}, error) {
+	return r.DBManager.RunTransaction(context.Background(), false, query)
+}
