bd274f88be231e1f3fce874a3817efa0dbf0179f
Marco Argentieri <3596602+tiero@users.noreply.github.com>
2021-06-11 13:20:41 +0200

    Update internal/interfaces/service.go

--- internal/interfaces/service.go
+++ internal/interfaces/service.go
@@ -1,7 +1,7 @@
 package interfaces
 
 // Service interface defines the methods that every kind of interface, whether
-// gRPC, REST, or whatever must be complaint with.
+// gRPC, REST, or whatever must be comply with.
 type Service interface {
 	Start(
 		operatorAddress, tradeAddress,
