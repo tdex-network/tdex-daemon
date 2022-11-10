49b6ca82bc4789195bd8ac0e9c66d0916e22ceda
altafan <18440657+altafan@users.noreply.github.com>
2022-11-10 19:02:35 +0100

    Move pluggable strategy to infrastructure

--- internal/infrastructure/market-strategy/pluggable/strategy.go
+++ internal/infrastructure/market-strategy/pluggable/strategy.go
@@ -1,7 +1,8 @@
-package domain
+package pluggable_strategy
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/shopspring/decimal"
 	"github.com/tdex-network/tdex-daemon/pkg/mathutil"
@@ -14,49 +15,57 @@ type PluggableStrategyOpts struct {
 	Fee        uint64
 }
 
+var (
+	ErrInvalidOptsType     = fmt.Errorf("opts must be of type PluggableStrategyOpts")
+	ErrPreviewAmountTooLow = fmt.Errorf("provided amount is too low")
+	ErrPreviewAmountTooBig = fmt.Errorf("provided amount is too big")
+)
+
 type PluggableStrategy struct{}
 
-func (s PluggableStrategy) SpotPrice(_opts interface{}) (spotPrice decimal.Decimal, err error) {
+func (s PluggableStrategy) SpotPrice(
+	_ interface{},
+) (spotPrice decimal.Decimal, err error) {
 	return
 }
 
-func (s PluggableStrategy) OutGivenIn(_opts interface{}, amountIn uint64) (uint64, error) {
+func (s PluggableStrategy) OutGivenIn(
+	_opts interface{}, amountIn uint64,
+) (uint64, error) {
 	opts, ok := _opts.(PluggableStrategyOpts)
 	if !ok {
-		return 0, errors.New("opts must be of type PluggableStrategyOpts")
+		return 0, ErrInvalidOptsType
 	}
 	if amountIn == 0 {
-		return 0, ErrMarketPreviewAmountTooLow
+		return 0, ErrPreviewAmountTooLow
 	}
 
 	amountR := decimal.NewFromInt(int64(amountIn)).Mul(opts.Price).BigInt().Uint64()
 	amountR, _ = mathutil.LessFee(amountR, opts.Fee)
 	if amountR == 0 {
-		return 0, ErrMarketPreviewAmountTooLow
+		return 0, ErrPreviewAmountTooLow
 	}
 	return amountR, nil
 }
 
-func (s PluggableStrategy) InGivenOut(_opts interface{}, amountOut uint64) (uint64, error) {
+func (s PluggableStrategy) InGivenOut(
+	_opts interface{}, amountOut uint64,
+) (uint64, error) {
 	opts, ok := _opts.(PluggableStrategyOpts)
 	if !ok {
 		return 0, errors.New("opts must be of type PluggableStrategyOpts")
 	}
 	if amountOut == 0 {
-		return 0, ErrMarketPreviewAmountTooLow
+		return 0, ErrPreviewAmountTooLow
 	}
 	if amountOut >= opts.BalanceOut {
-		return 0, ErrMarketPreviewAmountTooBig
+		return 0, ErrPreviewAmountTooBig
 	}
 
 	amountP := decimal.NewFromInt(int64(amountOut)).Mul(opts.Price).BigInt().Uint64()
 	amountP, _ = mathutil.PlusFee(amountP, opts.Fee)
 	if amountP == 0 {
-		return 0, ErrMarketPreviewAmountTooLow
+		return 0, ErrPreviewAmountTooLow
 	}
 	return amountP, nil
-}
-
-func (s PluggableStrategy) FormulaType() int {
-	return int(StrategyTypePluggable)
 }
