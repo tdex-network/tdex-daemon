1b66a47fae78eb04d84934cf161d7f2053767fc3
altafan <18440657+altafan@users.noreply.github.com>
2022-11-10 18:58:08 +0100

    Remove deprecated repos and transaction stuf

--- internal/core/ports/db.go
+++ internal/core/ports/db.go
@@ -1,44 +1,15 @@
 package ports
 
 import (
-	"context"
-
 	"github.com/tdex-network/tdex-daemon/internal/core/domain"
 )
 
 // RepoManager interface defines the methods for swap, price and unspent.
 type RepoManager interface {
-	VaultRepository() domain.VaultRepository
 	MarketRepository() domain.MarketRepository
-	UnspentRepository() domain.UnspentRepository
 	TradeRepository() domain.TradeRepository
 	DepositRepository() domain.DepositRepository
 	WithdrawalRepository() domain.WithdrawalRepository
 
 	Close()
-
-	NewTransaction() Transaction
-	NewPricesTransaction() Transaction
-	NewUnspentsTransaction() Transaction
-	RunTransaction(
-		ctx context.Context,
-		readOnly bool,
-		handler func(ctx context.Context) (interface{}, error),
-	) (interface{}, error)
-	RunUnspentsTransaction(
-		ctx context.Context,
-		readOnly bool,
-		handler func(ctx context.Context) (interface{}, error),
-	) (interface{}, error)
-	RunPricesTransaction(
-		ctx context.Context,
-		readOnly bool,
-		handler func(ctx context.Context) (interface{}, error),
-	) (interface{}, error)
-}
-
-// Transaction interface defines the method to commit or discard a database transaction.
-type Transaction interface {
-	Commit() error
-	Discard()
 }
