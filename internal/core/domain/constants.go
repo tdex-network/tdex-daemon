0fb0529ad52f2dfaf164ecef2479b8c3c92b9cf5
altafan <18440657+altafan@users.noreply.github.com>
2023-04-03 16:31:53 +0200

    Fixes

--- internal/core/domain/constants.go
+++ internal/core/domain/constants.go
@@ -5,21 +5,27 @@ const (
 	StrategyTypePluggable
 	StrategyTypeBalanced
 	StrategyTypeUnbalanced
+)
 
+const (
 	TradeStatusCodeUndefined = iota
 	TradeStatusCodeProposal
 	TradeStatusCodeAccepted
 	TradeStatusCodeCompleted
 	TradeStatusCodeSettled
 	TradeStatusCodeExpired
+)
 
+const (
+	TradeBuy TradeType = iota
+	TradeSell
+)
+
+const (
 	MinPercentageFee = 0
 	MaxPercentageFee = 9999
 
 	FeeAccount              = "fee_account"
 	FeeFragmenterAccount    = "fee_fragmenter_account"
 	MarketFragmenterAccount = "market_fragmenter_account"
-
-	TradeBuy TradeType = iota
-	TradeSell
 )
