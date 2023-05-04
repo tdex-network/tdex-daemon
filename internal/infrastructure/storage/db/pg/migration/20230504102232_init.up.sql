9c7a7d952ac8d9bff82d3ae570fd09b599fe00ea
sekulicd <sekula87@gmail.com>
2023-05-04 16:05:58 +0200

    refactor after review

--- internal/infrastructure/storage/db/pg/migration/20230504102232_init.up.sql
+++ internal/infrastructure/storage/db/pg/migration/20230504102232_init.up.sql
@@ -1,13 +1,13 @@
 CREATE TABLE market (
     name VARCHAR(50) NOT NULL PRIMARY KEY,
-    base_asset VARCHAR(255) NOT NULL,
-    quote_asset VARCHAR(255) NOT NULL,
-    base_asset_precision INTEGER NOT NULL DEFAULT 8,
-    quote_asset_precision INTEGER NOT NULL DEFAULT 8,
-    tradeable BOOLEAN NOT NULL DEFAULT FALSE,
-    strategy_type INTEGER NOT NULL DEFAULT 0,
-    base_price INTEGER NOT NULL DEFAULT 0,
-    quote_price INTEGER NOT NULL DEFAULT 0,
+    base_asset VARCHAR(64) NOT NULL,
+    quote_asset VARCHAR(64) NOT NULL,
+    base_asset_precision INTEGER,
+    quote_asset_precision INTEGER,
+    tradable BOOLEAN,
+    strategy_type INTEGER,
+    base_price INTEGER,
+    quote_price INTEGER,
     UNIQUE (base_asset, quote_asset)
 );
 
@@ -25,14 +25,14 @@ CREATE TABLE trade (
     type INTEGER NOT NULL,
     fee_asset VARCHAR(255) NOT NULL,
     fee_amount BIGINT NOT NULL,
-    trader_pubkey BYTEA NOT NULL,
-    status_code INTEGER NOT NULL DEFAULT 0,
-    status_failed BOOLEAN NOT NULL DEFAULT FALSE,
+    trader_pubkey BYTEA,
+    status_code INTEGER,
+    status_failed BOOLEAN,
     pset_base64 TEXT NOT NULL,
-    tx_id VARCHAR(255) NOT NULL,
+    tx_id VARCHAR(64),
     tx_hex TEXT NOT NULL,
-    expiry_time BIGINT NOT NULL,
-    settlement_time BIGINT NOT NULL,
+    expiry_time BIGINT,
+    settlement_time BIGINT,
     fk_market_name VARCHAR(50) NOT NULL,
     FOREIGN KEY (fk_market_name) REFERENCES market (name)
 );
@@ -49,15 +49,15 @@ CREATE TABLE swap (
 CREATE TABLE transaction (
     id SERIAL PRIMARY KEY,
     type VARCHAR(10) NOT NULL CHECK (type IN ('withdrawal', 'deposit')),
-    account_name TEXT NOT NULL,
-    tx_id TEXT NOT NULL,
+    account_name VARCHAR(50) NOT NULL,
+    tx_id VARCHAR(64) NOT NULL,
     timestamp BIGINT NOT NULL
 );
 
 CREATE TABLE transaction_asset_amount (
     id SERIAL PRIMARY KEY,
-    transaction_id INTEGER NOT NULL,
-    asset TEXT NOT NULL,
+    fk_transaction_id INTEGER NOT NULL,
+    asset VARCHAR(64) NOT NULL,
     amount BIGINT NOT NULL,
-    FOREIGN KEY (transaction_id) REFERENCES transaction (id)
+    FOREIGN KEY (fk_transaction_id) REFERENCES transaction (id)
 );
